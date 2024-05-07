2e4965e5a7b3a42db17b89447725061a3603d9ac
clivissonjose <cjbarbosa2001@gmail.com>
2024-05-07 15:36:25 -0300

    estudos sobre documentacao e terminal java

---
+++ src/documentacao/Calculadora.java
@@ -0,0 +1,21 @@
+package documentacao;
+
+/**
+ * 
+ * @author Clivisson barbosa
+ * @since 05/05/2024
+ * @version 1.0
+ */
+
+public class Calculadora {
+/**
+ * 
+ * @param numero1 primeiro parâmetro da classe (inteiro)
+ * @param numero2 segundo parâmetro do método
+ * @param return Retorna o valor da soma entre os parâmetro numero11 e numero2
+ */
+	
+	public int soma(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+}
---
+++ src/javaTerminal/MinhaClasse.java
@@ -0,0 +1,11 @@
+package javaTerminal;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world, i am being executed by terminal");
+
+	}
+
+}
