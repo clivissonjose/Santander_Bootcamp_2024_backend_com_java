e8e4f2425ce8c78e71ca023e8d03c9f179d0f8bf
clivissonjose <cjbarbosa2001@gmail.com>
2024-05-07 19:06:44 -0300

    terminal java e argumentos da classe main

---
+++ src/argumentos/MainArgumentos.java
@@ -0,0 +1,11 @@
+package argumentos;
+
+public class MainArgumentos {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo!");
+
+	}
+
+}
--- src/javaTerminal/MinhaClasse.java
+++ src/javaTerminal/MinhaClasse.java
@@ -5,6 +5,7 @@ public class MinhaClasse {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world, i am being executed by terminal");
+		System.out.println("Hello world");
 
 	}
 
